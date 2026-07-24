f3b94f3127eed032f34b09566fdd818e27d3cda3
zesciati <zakyabiyyu2003@gmail.com>
2026-07-24 14:53:17 +0700

    hero image ideas done

--- src/App.tsx
+++ src/App.tsx
@@ -18,24 +18,14 @@ function App() {
       </nav>
 
       <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage:
-            "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
-        }}
+        className="hero-bg-image min-h-100 flex items-center justify-center bg-[url(/images/pexels-pixabay.jpeg)]" 
       >
-        <div className="hero-overlay"></div>
-        <div className="hero-content text-neutral-content text-center">
-          <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+          <div className="flex-col text-center items-center text-white">
+            <h1 className="mb-5 text-5xl font-bold">Ideas</h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
+              Where all our great things begin
             </p>
-            <button className="btn btn-primary">Get Started</button>
           </div>
-        </div>
       </div>
     </>
   );
